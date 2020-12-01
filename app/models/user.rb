8b2b6f09a05a13c991e342aa7e3a96050d1b2dd3
hikaru-udon <mare.nozomu512@gmail.com>
2020-12-01 23:20:13 +0900

    validatesの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  validates :password, format: { with: /\A[a-z0-9]+\z/i }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day, presence: true
 end
