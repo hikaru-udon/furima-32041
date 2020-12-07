5c1d6b564a54a1a91a95af54c6261c45c6b345b1
hikaru-udon <mare.nozomu512@gmail.com>
2020-12-07 20:56:12 +0900

    レビュー後　修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,6 @@ class Item < ApplicationRecord
 
   validates :image, :name, :explanation, :price, presence: true
   validates :price, format: { with: /\A[0-9]+\z/}
-  validates :price, numericality: { greater_than_or_equal_to: 300 }
-  validates :price, numericality: { less_than_or_equal_to: 9999999 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_date_id,  numericality: { other_than: 1 }
 end
