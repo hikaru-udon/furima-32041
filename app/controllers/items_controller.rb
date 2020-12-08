51d0f6a41c04d611a61a63c2d4531f5c8e663ac0
hikaru-udon <mare.nozomu512@gmail.com>
2020-12-08 19:27:21 +0900

    edit updateのコントローラアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,20 @@ before_action :authenticate_user!, except: [:index, :show]
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      item = Item.find(params[:id])
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
