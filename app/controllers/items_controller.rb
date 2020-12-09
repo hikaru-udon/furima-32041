bc2c45253c91b90cd0de8afbd87c2f6d29e58a24
hikaru-udon <mare.nozomu512@gmail.com>
2020-12-09 14:53:12 +0900

    destroyコントローラーの設定＆出品者のみ削除可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 before_action :authenticate_user!, except: [:index, :show]
-before_action :move_to_index, only: [:edit]
+# before_action :move_to_index, only: [:edit, :destroy]　dstroyアクションはパスが重複しているかつ、直打ちで確認はできない。
 before_action :set_item, only: [:show, :edit, :update]
 
   def index
@@ -33,17 +33,26 @@ before_action :set_item, only: [:show, :edit, :update]
       render :edit
     end
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    if current_user.id == item.user_id
+      item.destroy
+      redirect_to root_path
+    end
+  end
   
   private
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
   
-  def move_to_index 
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  #メモ：authenticate_user!で先にサインイン画面に弾いているので、必要がない。
+  # def move_to_index 
+  #   unless user_signed_in?
+  #     redirect_to action: :index
+  #   end
+  # end
   
   def set_item
     @item = Item.find(params[:id])
