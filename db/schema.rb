4fb116bfdf43d73cf0001df70f889ff45a34e387
hikaru-udon <mare.nozomu512@gmail.com>
2020-12-12 18:25:57 +0900

    addressテーブルのカラム名を修正

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_12_11_093943) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "number", null: false
-    t.string "buildeing", null: false
+    t.string "building", null: false
     t.string "phone_number", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
