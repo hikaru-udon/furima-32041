7f9ecf5b28df28dde2a4e49db5e756652ac15e39
hikaru-udon <mare.nozomu512@gmail.com>
2020-12-14 13:03:13 +0900

    item_price.jsを一部修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,22 @@
-window.addEventListener('load', () => {
+function feeProfit(){
   
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+  priceInput.addEventListener('input', function(){
+    const inputValue = priceInput.value
 
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
 
     const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML)
+    profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
 
-  })
+  });
 
-});
+};
+
+window.addEventListener('load', feeProfit)
+
+// const url = location.pathname
+// if (url ==("/items/new"){
+
+// }
